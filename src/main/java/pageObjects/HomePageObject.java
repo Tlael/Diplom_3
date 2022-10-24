77c676c53c23e47d7a94e93c2715a8511ebc59a7
Tlael <Shurtana@gmail.com>
2022-10-24 19:29:11 +0300

    Add files via upload

--- src/main/java/pageObjects/HomePageObject.java
+++ src/main/java/pageObjects/HomePageObject.java
@@ -4,18 +4,17 @@ import io.qameta.allure.Step;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
 
 public class HomePageObject extends BasePageObject {
     private final By loginButton = By.xpath("//button[text()='Войти в аккаунт']");
     private final By orderButton = By.xpath("//button[text()='Оформить заказ']");
-
     private final By bun = By.xpath("//span[text() = 'Булки']");
     private final By sauce = By.xpath("//span[text() = 'Соусы']");
     private final By filling = By.xpath("//span[text() = 'Начинки']");
     private final By bunSelected = By.xpath("//span[text()='Булки']/..");
     private final By sauceSelected = By.xpath("//span[text()='Соусы']/..");
     private final By fillingSelected = By.xpath("//span[text()='Начинки']/..");
+    private final By constructor = By.xpath("//div[@style='display: flex;']/div");
 
     public HomePageObject(WebDriver driver) {
         super(driver);
@@ -25,26 +24,34 @@ public class HomePageObject extends BasePageObject {
         return driver.findElement(orderButton);
     }
 
-    public String getBunClassName() {
-        driver.findElement(sauce).click();
-        driver.findElement(bun).click();
-        return driver.findElement(bunSelected).getAttribute("class");
+    private WebElement getLoginButton() {
+        return driver.findElement(loginButton);
     }
 
+    @Step("Return current class attribute")
+    public String getCurrentClassAttributeValue() {
+        return "tab_tab__1SPyG tab_tab_type_current__2BEPc pt-4 pr-10 pb-4 pl-10 noselect";
+    }
+
+    @Step("Get Bun Class name")
+    public String getBunClassName() {
+        driver.findElement(filling).click();
+        driver.findElement(bun).click();
+        return driver.findElement(constructor).findElement(bunSelected).getAttribute("class");
+    }
+
+    @Step("Get Sauce Class name")
     public String getSauceClassName() {
         driver.findElement(filling).click();
         driver.findElement(sauce).click();
-        return driver.findElement(sauceSelected).getAttribute("class");
+        return driver.findElement(constructor).findElement(sauceSelected).getAttribute("class");
     }
 
+    @Step("Get Filling Class name")
     public String getFillingClassName() {
         driver.findElement(sauce).click();
         driver.findElement(filling).click();
-        return driver.findElement(fillingSelected).getAttribute("class");
-    }
-
-    private WebElement getLoginButton() {
-        return driver.findElement(loginButton);
+        return driver.findElement(constructor).findElement(fillingSelected).getAttribute("class");
     }
 
     @Step("Press Login button")
--- src/test/java/BurgerConstructorTest.java
+++ src/test/java/BurgerConstructorTest.java
@@ -20,24 +20,21 @@ public class BurgerConstructorTest extends StartUp {
     @DisplayName("Check Buns")
     public void bunsTabTest() {
         driver.get(HOME_PAGE_URL);
-        String classNameAfterSelected = new HomePageObject(driver).getBunClassName();
-        assertEquals(home.getBunClassName(), classNameAfterSelected);
+        assertEquals(home.getBunClassName(), home.getCurrentClassAttributeValue());
     }
 
     @Test
     @DisplayName("Check sauce")
     public void sauceTabTest() {
         driver.get(HOME_PAGE_URL);
-        String classNameAfterSelected = new HomePageObject(driver).getSauceClassName();
-        assertEquals(home.getSauceClassName(), classNameAfterSelected);
+        assertEquals(home.getSauceClassName(), home.getCurrentClassAttributeValue());
     }
 
     @Test
     @DisplayName("Check filling")
     public void fillingTabTest() {
         driver.get(HOME_PAGE_URL);
-        String classNameAfterSelected = new HomePageObject(driver).getFillingClassName();
-        assertEquals(home.getFillingClassName(), classNameAfterSelected);
+        assertEquals(home.getFillingClassName(), home.getCurrentClassAttributeValue());
     }
 
     @After
